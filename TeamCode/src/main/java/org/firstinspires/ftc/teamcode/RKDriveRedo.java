3db9c06f7fadff244da45655aba2afd8e09aae2c
LAPTOP-T00AO7D5\Bodie Woods <Bodiewolf26@gmail.com>
2020-09-27 00:32:59 -0700

    no message

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RKDriveRedo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RKDriveRedo.java
@@ -6,8 +6,8 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-@TeleOp(name = "RKdrive")
-public class RKDrive extends LinearOpMode {
+@TeleOp(name = "RKdrive redo")
+public class RKDriveRedo extends LinearOpMode {
     private DcMotor motorFrontRight;
     private DcMotor motorFrontLeft;
     private DcMotor motorBackRight;
@@ -28,49 +28,46 @@ public class RKDrive extends LinearOpMode {
         motorBackRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         motorBackLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
+        double left = 0;
+        double right = 0;
+
         waitForStart();
         waitForStart();
 
         while (opModeIsActive()) {
 
 
+            motorFrontRight.setPower(right);
+            motorBackRight.setPower(right);
+            motorFrontLeft.setPower(left);
+            motorBackLeft.setPower(left);
+
+
             if (gamepad1.left_stick_y > 0.2 || gamepad1.left_stick_y < -0.2){
                 while (gamepad1.left_stick_y > 0.2 || gamepad1.left_stick_y < -0.2) {
-                    motorFrontRight.setPower(-gamepad1.left_stick_y / 2);
-                    motorBackRight.setPower(-gamepad1.left_stick_y / 2);
-                    motorFrontLeft.setPower(-gamepad1.left_stick_y / 2);
-                    motorBackLeft.setPower(-gamepad1.left_stick_y / 2);
+                    left = left - gamepad1.left_stick_y;
+                    right = right - gamepad1.left_stick_y;
                 }} else{
-                motorFrontRight.setPower(0);
-                motorBackRight.setPower(0);
-                motorFrontLeft.setPower(0);
-                motorBackLeft.setPower(0);
+                left = 0;
+                right = 0;
             }
 
             if (gamepad1.right_stick_x > 0.2){
                 while (gamepad1.right_stick_x > 0.2) {
-                    motorFrontRight.setPower(gamepad1.right_stick_x / 2);
-                    motorBackRight.setPower(gamepad1.right_stick_x / 2);
-                    motorFrontLeft.setPower(-gamepad1.right_stick_x / 2);
-                    motorBackLeft.setPower(-gamepad1.right_stick_x / 2);
+                    right = right + gamepad1.left_stick_y;
+                    left = left - gamepad1.left_stick_y;
                 }} else{
-                motorFrontRight.setPower(0);
-                motorBackRight.setPower(0);
-                motorFrontLeft.setPower(0);
-                motorBackLeft.setPower(0);
+                left = 0;
+                right = 0;
             }
 
             if (gamepad1.right_stick_x < -0.2) {
                 while (gamepad1.right_stick_x < -0.2) {
-                    motorFrontRight.setPower(gamepad1.right_stick_x / 2);
-                    motorBackRight.setPower(gamepad1.right_stick_x / 2);
-                    motorFrontLeft.setPower(-gamepad1.right_stick_x / 2);
-                    motorBackLeft.setPower(-gamepad1.right_stick_x / 2);
+                    right = right + gamepad1.left_stick_y;
+                    left = left - gamepad1.left_stick_y;
                 } } else {
-                motorFrontRight.setPower(0);
-                motorBackRight.setPower(0);
-                motorFrontLeft.setPower(0);
-                motorBackLeft.setPower(0);
+                left = 0;
+                right = 0;
             }
 
             if (gamepad1.left_stick_x > 0.2){
