7e9f11a639b2afed97948c74a8d70e24496ed0eb
“misterjonny” <jaibel@pacbell.net>
2021-01-21 22:01:04 -1000

    no message

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleAuton.java
@@ -187,12 +187,7 @@ public class SampleAuton extends LinearOpMode {
         }
 
         int rings = 0;
-
-<<<<<<< HEAD
-
-
-=======
->>>>>>> d7d3660dedf8c046e63b4136c734c374ce3b31ec
+        
         //detect using camera
         boolean ison = true;
 
